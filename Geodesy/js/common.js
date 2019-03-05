4d1d1b211cc9fd58651d07b109df06cbf1987473
DeNikitin <windrider25@yandex.ru>
2019-03-05 16:28:54 +0300

    Commit Geodesy

---
+++ Geodesy/js/common.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+ $('.slow').on("click", function (event) {
+                //отменяем стандартную обработку нажатия по ссылке
+                event.preventDefault();
+
+                //забираем идентификатор бока с атрибута href
+                var id  = $(this).attr('href'),
+
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                    top = $(id).offset().top;
+
+                //анимируем переход на расстояние - top за 1500 мс
+                $('body,html').animate({scrollTop: top}, 1500);
+            });
+
+ $("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+ $('#menu-menu-1 a').each(function(index){
+     if ($(this).attr('href') == window.location) {
+        $(this).css({'background':'linear-gradient(#f4893b, #ef5f31)'});
+    }
+    });
+
+  $('.popup').magnificPopup({
+    type:'inline'
+}); 
+
+  $("form").submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "send.php",
+            data: $(this).serialize()
+        }).done(function() {
+            alert("Спасибо за заявку!");
+            setTimeout(function() {
+                $.magnificPopup.close();
+            }, 1000);
+        });
+        return false;
+    });
+
+$(".wrapper .tab_item").not(":first").hide();
+$(".wrapper .tab").click(function() {
+    $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".wrapper .tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
+
+});
+
+
+
+
+
